155b938ce762a89ea285319e94c082e5b2993747
26june <gj.abordo1@gmail.com>
2023-10-27 13:55:33 +0100

    proj start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="appHome"></div>
+      <div className="appAbout"></div>
+      <div className="appProject"></div>
+    </div>
+  );
+}
+
+export default App;
